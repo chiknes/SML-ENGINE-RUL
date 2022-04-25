811c115f4cae1faede7fd0c2d7055835e446fa9d
chiknes <shailesh.vit@gmail.com>
2022-04-25 19:03:51 -0400

    Classifier and CNN added

--- Preprocessing/pre_processing.py
+++ Preprocessing/pre_processing.py
@@ -1,18 +1,15 @@
+import sys
+
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_selection import RFE
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LinearRegression
-import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 from pylab import rcParams
-import math
-import xgboost
-import time
-from tqdm import tqdm
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 data = pd.read_csv('../CMAPSSData/train_FD001.txt', sep=' ', header=None)
 test_data = pd.read_csv('../CMAPSSData/test_FD001.txt', sep=' ', header=None)
@@ -20,7 +17,8 @@ test_data = pd.read_csv('../CMAPSSData/test_FD001.txt', sep=' ', header=None)
 # NaN values only in these 2 columns
 data.drop(columns=[26, 27], inplace=True)
 test_data.drop(columns=[26, 27], inplace=True)
-columns = ['unit_number', 'time_in_cycles', 'setting_1', 'setting_2', 'setting_3']
+columns = ['unit_number', 'time_in_cycles',
+           'setting_1', 'setting_2', 'setting_3']
 
 for i in range(1, 22):
     columns.append('sensor_' + str(i))
@@ -40,60 +38,268 @@ def prepare_training_data(data, factor=0):
 
     return df[df['time_in_cycles'] > factor]
 
-
 training_data = prepare_training_data(data)
-# print(df)
-# sns.heatmap(training_data.corr(), annot=True, cmap='RdYlGn', linewidths=0.2)
-# fig = plt.gcf()
-# fig.set_size_inches(25, 25)
-# plt.show()
 
-X_train = training_data.iloc[:, 0:26]
-# print(X_train)
-y_train = training_data['RUL']
-lm = RandomForestRegressor(n_estimators=70, max_depth=5, n_jobs=-1, random_state=1)
-rfe = RFE(lm)  # running RFE
-rfe = rfe.fit(X_train, y_train)
-print(rfe.support_)  # Printing the boolean results
-print(rfe.ranking_)
-print(rfe.estimator_)
-print(rfe.n_features_)
+TTF = 10
+training_data['label'] = np.where(training_data['RUL'] <= TTF, 1, 0 )
 
-plt.figure(figsize = (16, 21))
+X_class = training_data.iloc[:,[6,7,8,11,12,13,15,16,17,18,19,21,24,25]].to_numpy()
+Y_class = training_data.iloc[:,27:].to_numpy()
+Y_class = np.ravel(Y_class)
 
-for i in range(21):
-    temp_data = training_data.iloc[:,i+5]
-    plt.subplot(7,3,i+1)
-    plt.boxplot(temp_data)
-    plt.title("Sensor " + str(i+1) + ", column "+ str(i+6))
-# plt.show()
+test_max = test_data.groupby('unit_number')['time_in_cycles'].max().reset_index()
+test_max.columns = ['unit_number','max']
+fd_001_test = test_data.merge(test_max, on=['unit_number'], how='left')
+test = fd_001_test[fd_001_test['time_in_cycles'] == fd_001_test['max']].reset_index()
+test.drop(columns=['index','max','unit_number','time_in_cycles','setting_1','setting_2','setting_3','sensor_1', 'sensor_5', 'sensor_6', 'sensor_10', 'sensor_16', 'sensor_18','sensor_19'],inplace = True)
+X_001_test = test.to_numpy()
 
 
-def train_models(data, model='FOREST'):
-    X = data.iloc[:, :14].to_numpy()
-    Y = data.iloc[:, 14:].to_numpy()
-    Y = np.ravel(Y)
-    if model == 'FOREST':
-        model = RandomForestRegressor(n_estimators=70, max_features=12, max_depth=5, n_jobs=-1, random_state=1)
-        model.fit(X, Y)
-        return model
-    elif model == 'XGB':
-        model = xgboost.XGBRegressor(n_estimators=110, learning_rate=0.018, gamma=0, subsample=0.8,
-                                     colsample_bytree=0.5, max_depth=3, silent=True)
-        model.fit(X, Y)
-        return model
+
+from imblearn.over_sampling import RandomOverSampler
+ros = RandomOverSampler(random_state=0)
+ros.fit(X_class, Y_class)
+X_resampled, y_resampled = ros._fit_resample(X_class, Y_class)
+print('Number of elements before re-sampling:', len(X_class))
+print('Number of elements after re-sampling:', len(X_resampled))
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X_resampled,y_resampled,test_size = 0.2,random_state = 3)
+
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+
+
+forest = RandomForestClassifier(n_estimators=70 ,max_depth = 8, random_state=193)
+forest.fit(X_train,y_train)
+
+model_xgb = XGBClassifier()
+model_xgb.fit(X_train, y_train)
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+def classificator_score(y_,y_p):
+    print(f' accuracy score {round(accuracy_score(y_, y_p),2)}')
+    print(f' precision score {round(precision_score(y_, y_p),2)}')
+    print(f' recall score {round(recall_score(y_, y_p),2)}')
+    print(f' F1 score {round(f1_score(y_, y_p),2)}')
     return
 
+classificator_score(y_test,forest.predict(X_test))
 
-def plot_result(y_true, y_pred):
-    rcParams['figure.figsize'] = 12, 10
-    plt.plot(y_pred)
-    plt.plot(y_true)
-    plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-    plt.ylabel('RUL')
-    plt.xlabel('training samples')
-    plt.legend(('Predicted', 'True'), loc='upper right')
-    plt.show()
-    return
+y_xgb_pred = model_xgb.predict(X_001_test)
+classificator_score(y_test,model_xgb.predict(X_test))
 
-# model_1 = train_models(training_data)
+columns_to_be_dropped = [0, 1, 2, 3, 4]
+train_data = pd.read_csv("../CMAPSSData/train_FD001.txt", sep="\s+", header=None)
+test_data = pd.read_csv("../CMAPSSData/test_FD001.txt", sep="\s+", header=None)
+true_rul = pd.read_csv("../CMAPSSData/RUL_FD001.txt", sep='\s+', header=None)
+
+window_length = 15
+shift = 1
+early_rul = 150
+processed_train_data = []
+processed_train_targets = []
+
+# How many test windows to take for each engine. If set to 1 (this is the default), only last window of test data for
+# each engine is taken. If set to a different number, that many windows from last are taken.
+# Final output is the average output of all windows.
+num_test_windows = 5
+processed_test_data = []
+num_test_windows_list = []
+
+train_data_first_column = train_data[0]
+test_data_first_column = test_data[0]
+
+# Scale data for all engines
+scaler = MinMaxScaler(feature_range=(-1, 1))
+train_data = scaler.fit_transform(train_data.drop(columns=columns_to_be_dropped))
+test_data = scaler.transform(test_data.drop(columns=columns_to_be_dropped))
+
+train_data = pd.DataFrame(data=np.c_[train_data_first_column, train_data])
+test_data = pd.DataFrame(data=np.c_[test_data_first_column, test_data])
+
+num_train_machines = len(train_data[0].unique())
+num_test_machines = len(test_data[0].unique())
+
+
+def process_targets(data_length, early_rul=None):
+    """
+    Takes datalength and earlyrul as input and
+    creates target rul.
+    """
+    if early_rul == None:
+        return np.arange(data_length - 1, -1, -1)
+    else:
+        early_rul_duration = data_length - early_rul
+        if early_rul_duration <= 0:
+            return np.arange(data_length - 1, -1, -1)
+        else:
+            return np.append(early_rul * np.ones(shape=(early_rul_duration,)), np.arange(early_rul - 1, -1, -1))
+
+
+def process_input_data_with_targets(input_data, target_data=None, window_length=1, shift=1):
+    """Depending on values of window_length and shift, this function generates batchs of data and targets
+    from input_data and target_data.
+
+    Number of batches = np.floor((len(input_data) - window_length)/shift) + 1
+
+    **We don't check input dimensions uisng exception handling. So readers should be careful while using these
+    functions. If input data are not of desired dimension, either error occurs or something undesirable is
+    produced as output.**
+
+    Arguments:
+        input_data: input data to function (Must be 2 dimensional)
+        target_data: input rul values (Must be 1D array)s
+        window_length: window length of data
+        shift: Distance by which the window moves for next batch. This is closely related to overlap
+               between data. For example, if window length is 30 and shift is 1, there is an overlap of
+               29 data points between two consecutive batches.
+
+    """
+    num_batches = int(np.floor((len(input_data) - window_length) / shift)) + 1
+    num_features = input_data.shape[1]
+    output_data = np.repeat(np.nan, repeats=num_batches * window_length * num_features).reshape(num_batches,
+                                                                                                window_length,
+                                                                                                num_features)
+    if target_data is None:
+        for batch in range(num_batches):
+            output_data[batch, :, :] = input_data[(0 + shift * batch):(0 + shift * batch + window_length), :]
+        return output_data
+    else:
+        output_targets = np.repeat(np.nan, repeats=num_batches)
+        for batch in range(num_batches):
+            output_data[batch, :, :] = input_data[(0 + shift * batch):(0 + shift * batch + window_length), :]
+            output_targets[batch] = target_data[(shift * batch + (window_length - 1))]
+        return output_data, output_targets
+
+
+def process_test_data(test_data_for_an_engine, window_length, shift, num_test_windows=1):
+    """ This function takes test data for an engine as first input. The next two inputs
+    window_length and shift are same as other functins.
+
+    Finally it takes num_test_windows as the last input. num_test_windows sets how many examplles we
+    want from test data (from last). By default it extracts only the last example.
+
+    The function return last examples and number of last examples (a scaler) as output.
+    We need the second output later. If we are extracting more than 1 last examples, we have to
+    average their prediction results. The second scaler halps us do just that.
+    """
+    max_num_test_batches = int(np.floor((len(test_data_for_an_engine) - window_length) / shift)) + 1
+    if max_num_test_batches < num_test_windows:
+        required_len = (max_num_test_batches - 1) * shift + window_length
+        batched_test_data_for_an_engine = process_input_data_with_targets(test_data_for_an_engine[-required_len:, :],
+                                                                          target_data=None,
+                                                                          window_length=window_length, shift=shift)
+        return batched_test_data_for_an_engine, max_num_test_batches
+    else:
+        required_len = (num_test_windows - 1) * shift + window_length
+        batched_test_data_for_an_engine = process_input_data_with_targets(test_data_for_an_engine[-required_len:, :],
+                                                                          target_data=None,
+                                                                          window_length=window_length, shift=shift)
+        return batched_test_data_for_an_engine, num_test_windows
+
+
+# Process training and test data sepeartely as number of engines in training and test set may be different.
+# As we are doing scaling for full dataset, we are not bothered by different number of engines in training and test set.
+
+# Process trianing data
+for i in np.arange(1, num_train_machines + 1):
+    temp_train_data = train_data[train_data[0] == i].drop(columns=[0]).values
+
+    # Verify if data of given window length can be extracted from training data
+    if (len(temp_train_data) < window_length):
+        print("Train engine {} doesn't have enough data for window_length of {}".format(i, window_length))
+        raise AssertionError("Window length is larger than number of data points for some engines. "
+                             "Try decreasing window length.")
+
+    temp_train_targets = process_targets(data_length=temp_train_data.shape[0], early_rul=early_rul)
+    data_for_a_machine, targets_for_a_machine = process_input_data_with_targets(temp_train_data, temp_train_targets,
+                                                                                window_length=window_length,
+                                                                                shift=shift)
+
+    processed_train_data.append(data_for_a_machine)
+    processed_train_targets.append(targets_for_a_machine)
+
+processed_train_data = np.concatenate(processed_train_data)
+processed_train_targets = np.concatenate(processed_train_targets)
+
+# Process test data
+for i in np.arange(1, num_test_machines + 1):
+    temp_test_data = test_data[test_data[0] == i].drop(columns=[0]).values
+
+    # Verify if data of given window length can be extracted from test data
+    if (len(temp_test_data) < window_length):
+        print("Test engine {} doesn't have enough data for window_length of {}".format(i, window_length))
+        raise AssertionError("Window length is larger than number of data points for some engines. "
+                             "Try decreasing window length.")
+
+    # Prepare test data
+    test_data_for_an_engine, num_windows = process_test_data(temp_test_data, window_length=window_length, shift=shift,
+                                                             num_test_windows=num_test_windows)
+
+    processed_test_data.append(test_data_for_an_engine)
+    num_test_windows_list.append(num_windows)
+
+processed_test_data = np.concatenate(processed_test_data)
+true_rul = true_rul[0].values
+
+# Shuffle training data
+index = np.random.permutation(len(processed_train_targets))
+processed_train_data, processed_train_targets = processed_train_data[index], processed_train_targets[index]
+
+print("Processed trianing data shape: ", processed_train_data.shape)
+print("Processed training ruls shape: ", processed_train_targets.shape)
+print("Processed test data shape: ", processed_test_data.shape)
+print("True RUL shape: ", true_rul.shape)
+
+target_scaler = MinMaxScaler(feature_range=(0, 1))
+processed_train_targets = target_scaler.fit_transform(processed_train_targets.reshape(-1, 1)).reshape(-1)
+
+
+def scheduler(epoch):
+    if epoch < 10:
+        return 0.001
+    else:
+        return 0.01
+
+
+window_length = 15
+
+
+def create_compiled_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv1D(512, 3, activation = "relu", input_shape = (window_length, processed_train_data.shape[2])),
+        tf.keras.layers.Conv1D(96, 5, activation = "relu"),
+        tf.keras.layers.Conv1D(32, 5, activation = "relu"),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(64, activation = "relu"),
+        tf.keras.layers.Dense(128, activation = "relu"),
+        tf.keras.layers.Dense(1)
+    ])
+    model.compile(loss = "mse", optimizer = tf.keras.optimizers.Adam(learning_rate=0.01))
+    return model
+
+
+
+processed_train_data, processed_val_data, processed_train_targets, processed_val_targets = train_test_split(
+    processed_train_data,
+    processed_train_targets,
+    test_size=0.2,
+    random_state=83)
+
+callback = tf.keras.callbacks.LearningRateScheduler(scheduler, verbose=1)
+
+model = create_compiled_model()
+history = model.fit(processed_train_data, processed_train_targets, epochs=30,
+                    validation_data=(processed_val_data, processed_val_targets),
+                    callbacks=callback,
+                    batch_size=64, verbose=2)
+
+rul_pred_scaled = model.predict(processed_test_data).reshape(-1)
+rul_pred = target_scaler.inverse_transform(rul_pred_scaled.reshape(-1, 1)).reshape(-1)
+
+preds_for_each_engine = np.split(rul_pred, np.cumsum(num_test_windows_list)[:-1])
+mean_pred_for_each_engine = [np.average(ruls_for_each_engine, weights=np.repeat(1 / num_windows, num_windows))
+                             for ruls_for_each_engine, num_windows in zip(preds_for_each_engine, num_test_windows_list)]
+RMSE = np.sqrt(mean_squared_error(true_rul, mean_pred_for_each_engine))
+print("RMSE: ", RMSE)
