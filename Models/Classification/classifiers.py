44d2d07cd5e7a189ec2fcf70e72a98a37007e271
rishabh <rishabh0495@gmail.com>
2022-04-26 02:16:00 -0400

    Confusion matrix

--- Models/Classification/classifiers.py
+++ Models/Classification/classifiers.py
@@ -1,10 +1,11 @@
 import numpy as np
 from imblearn.over_sampling import RandomOverSampler
+from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
-
+import seaborn as sns
 from Preprocessing.pre_processing import test_data
 from Preprocessing.pre_processing import training_data
 
@@ -19,8 +20,8 @@ test_max = test_data.groupby('unit_number')['time_in_cycles'].max().reset_index(
 test_max.columns = ['unit_number', 'max']
 fd_001_test = test_data.merge(test_max, on=['unit_number'], how='left')
 test = fd_001_test[fd_001_test['time_in_cycles'] == fd_001_test['max']].reset_index()
-test.drop(columns=['index', 'max', 'unit_number', 'time_in_cycles', 'setting_1', 'setting_2', 'setting_3', 'sensor_1',
-                   'sensor_5', 'sensor_6', 'sensor_10', 'sensor_16', 'sensor_18', 'sensor_19'], inplace=True)
+test.drop(columns=['index', 'max', 'unit_number', 'time_in_cycles', 'setting_1', 'setting_2', 'TRA', 'T2',
+                   'P2', 'P15', 'epr', 'farB', 'Nf_dmd', 'PCNfR_dmd'], inplace=True)
 X_001_test = test.to_numpy()
 
 ros = RandomOverSampler(random_state=0)
@@ -50,3 +51,15 @@ classificator_score(y_test, forest.predict(X_test))
 
 y_xgb_pred = model_xgb.predict(X_001_test)
 classificator_score(y_test, model_xgb.predict(X_test))
+
+
+def build_confusion_matrix(actual, predicted):
+    plt.figure(figsize=(5, 5))
+    sns.heatmap(confusion_matrix(actual, predicted), annot=True, fmt='.5g')
+    plt.ylabel('Actual class')
+    plt.xlabel('Predicted class')
+    plt.show()
+
+build_confusion_matrix(y_test, forest.predict(X_test))
+
+
